66d31f5b15b2280d880b758dd5726044020da953
mas5535 <masoudtest2024@gmail.com>
2026-09-25 23:55:37 +0330

    Update service.py

--- service.py
+++ service.py
@@ -22,7 +22,7 @@ from datetime import datetime
 # تنظیمات
 # ============================================================
 STATE_FILE = "alerted.json"
-ACCUM_THRESHOLD = 50
+ACCUM_THRESHOLD = 55
 PUMP_THRESHOLD = 70
 
 
